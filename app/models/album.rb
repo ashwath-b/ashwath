6b1ed6ee8d914b6e61ed3d6abf0663cda0ca3a1d
ashwath-cp <ashwath@coffeepass.com>
2017-03-30 17:54:04 +0530

    Task #6, validate publisher on album save

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,6 +5,8 @@ class Album < ActiveRecord::Base
   validates :name, presence: true, length: { in: 2..50 }
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
+  validates :publisher_id, presence: true
+
   scope :most_recent, ->(num) { order('released_on DESC').limit(num) }
   scope :recent, ->(num) { where('released_on > ?', Time.now - 14.weeks).limit(num).order('released_on DESC') }
 end
