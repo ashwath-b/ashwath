498814b72b12917e4b3aab22eeeedad832dda39d
ashwath-cp <ashwath@coffeepass.com>
2017-03-30 17:50:46 +0530

    Task #5, replace the  text field with a dropdown

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,7 +1,7 @@
 class Publisher < ActiveRecord::Base
   has_many :albums
   has_many :songs, through: :albums
-  
+
   validates :name, length: { in: 5..50 }, uniqueness: true
   validates :address, length: { in: 5..50 }
   validates :city, length: { in: 5..50 }
@@ -9,4 +9,8 @@ class Publisher < ActiveRecord::Base
   validates :zip, format: { with: /\A\d{5}\z/, message: "should be five digits" }
   validates :phone, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, message: "should look like 210-555-1234" }
   validates :url, format: { with: /\Ahttp:\/\/.+\z/, message: "should look like http://www.yahoo.com" }
+
+  def name_with_phone
+    "#{name} - #{phone}"
+  end
 end
