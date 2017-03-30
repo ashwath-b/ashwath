e5dc33faaeb936df6bf7ddcb331b169d404579ff
ashwath-cp <ashwath@coffeepass.com>
2017-03-30 17:44:37 +0530

    Task #4,  scope

--- app/models/album.rb
+++ app/models/album.rb
@@ -6,5 +6,5 @@ class Album < ActiveRecord::Base
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
   scope :most_recent, ->(num) { order('released_on DESC').limit(num) }
-
+  scope :recent, ->(num) { where('released_on > ?', Time.now - 14.weeks).limit(num).order('released_on DESC') }
 end
